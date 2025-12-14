bb49935a278ff4c2a07b9efe1b248592c50b7ff6
Kirtimani Tamang <kirtimanitamang@gmail.com>
2025-12-14 07:07:44 +0545

    Initial sharing of project

---
+++ scenario6.java
@@ -0,0 +1,30 @@
+
+/**
+ * Mini Book Corner
+ *
+ * K
+ * v1.0
+ */
+public class scenario6
+{
+    public static void main(String[] args) 
+    {
+        String[] categories = {"Fiction", "Nepali"};
+
+        String[][] titles = new String[2][1];
+        double[][] prices = new double[2][1];
+
+        titles[0][0] = "Asahamati";
+        prices[0][0] = 750.0;
+        
+        titles[1][0] = "Muna Madan";
+        prices[1][0] = 500.0;
+
+        for (int i = 0; i < categories.length; i++) 
+        {
+            System.out.println("Category: " + categories[i]);
+            System.out.println("Title: " + titles[i][0]);
+            System.out.printf("Price: %.2f\n\n", prices[i][0]); // %.2f hereis used to show the last two digit of the output.
+        }
+    }
+}
